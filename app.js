12c74ed18007bc8d624c053f14f61794f8abb1ca
Kriz <kriz@live.com>
2015-10-08 22:46:38 +0300

    added jade

--- app.js
+++ app.js
@@ -1,23 +1,39 @@
 var express = require('express');
 var app = express();
-var config = require('./config');
 
-var status = null;
+app.set('view engine', 'jade');
+app.set('views', __dirname);
+
+var config = require('./config');
+var status = 'doing nothing';
+
 app.get('/', function(req, res) {
-  res.json({
+  res.render('./index', {
     status: status
   });
 });
 
-app.get('/status/:key/:status', function(req, res) {
+//set status
+app.get('/status/:key?/:status?', function(req, res) {
   var data = {
     ok: 0
   };
 
-  if (req.params.key === config.key) {
-    status = req.params.status;
+  var key = req.params.key;
+
+  if (key) {
+    if (key === config.key) {
+      status = req.params.status;
+      data.ok = 1;
+    } else {
+      data.ok = 0;
+      data.err = 'Auth Fail!';
+    }
+  } else {
     data.ok = 1;
+    data.status = status;
   }
+
 
   res.json(data);
 });
