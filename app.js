366f748bed086a08563ed4de87c585a08ed2d52e
Kriz <kriz@live.com>
2015-11-03 13:03:28 +0200

    sms notifications

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
-
-
+var moment = require('moment-timezone');
+moment.tz.setDefault("Europe/Tallinn");
 app.set('view engine', 'jade');
 app.set('views', __dirname);
 app.use(express.static('public'));
@@ -15,6 +15,10 @@ var today = new Date();
 var startDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() - 7).toISOString();
 var endDate =  (new Date()).toISOString();
 
+
+var twilio = require('twilio');
+var twilioClient = new twilio.RestClient(config.twilio.sid, config.twilio.token);
+
 var togglHours = 0;
 var getTogglStats = function() {
   toggl.getTimeEntries(startDate, endDate, function(err, timeEntries) {
@@ -26,7 +30,7 @@ var getTogglStats = function() {
     var totalDurationInHours = totalDuration  / 60 / 60;
     togglHours = totalDurationInHours;
   });
-}
+};
 
 getTogglStats();
 setInterval(getTogglStats, 10000);
@@ -43,7 +47,7 @@ var getPhrase = function(text) {
   }
 
   return phrase;
-}
+};
 
 app.get('/', function(req, res) {
   db.find({})
@@ -69,7 +73,6 @@ app.get('/', function(req, res) {
 var Datastore = require('nedb');
 var db = new Datastore({ filename: __dirname + '/database', autoload: true });
 
-
 //set status
 app.get('/status/:key?/:status?', function(req, res) {
   var data = {
@@ -80,12 +83,51 @@ app.get('/status/:key?/:status?', function(req, res) {
 
   if (key) {
     if (key === config.key) {
-      var status = req.params.status;
+      var status = req.params.status || 'no';
       data.ok = 1;
 
-      db.insert({
-        status: status,
-        date: new Date()
+      db.findOne({})
+      .sort({ date: -1})
+      .exec(function (err, doc) {
+        var lastStatus = doc.status;
+
+        db.insert({
+          status: status,
+          date: new Date()
+        });
+
+        if (lastStatus !== status && status === 'yes') {
+          var upperLimit = moment()
+          .hours(12)
+          .minutes(0);
+
+          var lowerLimit = moment()
+          .hours(9)
+          .minutes(0);
+
+          var now = moment();
+
+          var isInTimeLimit = now.isBetween(lowerLimit, upperLimit);
+
+          if (isInTimeLimit) {
+            var currentTime = moment().format('HH:mm');
+            console.log('SENDING MESSAGE');
+            console.log(`${config.user.name}@work ${currentTime}`);
+
+            twilioClient.messages.create({
+                to: config.twilio.to,
+                from: config.twilio.from,
+                body: `${config.user.name}@work ${currentTime}`,
+            }, function(error, message) {
+                if (error) {
+                    console.log(error.message);
+                }
+            });
+
+          } else {
+            console.info('Too Late');
+          }
+        }
       });
 
     } else {
